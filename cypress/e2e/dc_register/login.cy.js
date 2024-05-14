ae69b8d5c36efe2c45e6b09f930c83aad5f25ba7
malikhassan25 <malik.hassan@stixor.com>
2024-05-14 13:14:54 +0500

    Update login.cy.js

--- cypress/e2e/dc_register/login.cy.js
+++ cypress/e2e/dc_register/login.cy.js
@@ -1,13 +1,1 @@
-describe('login Automation Test', () => {
-    beforeEach(() => {
-        // Visit the Register page before each test
-        cy.visit('/login');
-    });
-    // Test Case for visibility of Input Fields and Sign-Up Button
-    it('visibility of Input Fields and Sign-Up Button', () => {
-        cy.get('[name="first_name"]').should('be.visible');
-        cy.get('[name="last_name"]').should('be.visible');
 
-        cy.get('.btn-submit1').should('be.visible');
-    });
-});
